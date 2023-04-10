4fb4ce0a8ca394408d83d75f2dc413822fd7f809
migueljuncalpz <miguelj@empathy.co>
2023-04-10 13:17:37 +0200

    Adding some changes on films store

--- src/stores/FilmStore.ts
+++ src/stores/FilmStore.ts
@@ -1,96 +1,207 @@
 import {defineStore} from "pinia";
-interface State {
-    filmsList: FilmInfo[]
-}
-export const useFilmStore = defineStore('film', {
-    state: (): State => {
-        return {
-            // for initially empty lists
-            filmsList: [
-                {
-                    filmId:1,
-                    filmImage: "https://static.euronews.com/articles/stories/07/31/01/20/606x758_cmsv2_dfdbec3d-af7a-5e75-9ae7-2ec0188cbf9a-7310120.jpg",
-                    name: 'Titanic',
-                    year: 1997,
-                    synopsys: 'James Cameron\'s "Titanic" is an epic, action-packed romance set against ... waters of the North Atlantic in the early hours of April 15, 1912.',
-                    gender:[],
-                    valueRating:7.9,
-                    ageRating:"+13",
-                    cast:[]
-                },
-                {
-                    filmId:2,
-                    filmImage: "https://static.euronews.com/articles/stories/07/31/01/20/606x758_cmsv2_dfdbec3d-af7a-5e75-9ae7-2ec0188cbf9a-7310120.jpg",
-                    name: 'Titanic2',
-                    year: 1997,
-                    synopsys: 'James Cameron\'s "Titanic" is an epic, action-packed romance set against ... waters of the North Atlantic in the early hours of April 15, 1912.',
-                    gender:[],
-                    valueRating:7.9,
-                    ageRating:"+13",
-                    cast:[]
-                },
-                {
-                    filmId:3,
-                    filmImage: "https://static.euronews.com/articles/stories/07/31/01/20/606x758_cmsv2_dfdbec3d-af7a-5e75-9ae7-2ec0188cbf9a-7310120.jpg",
-                    name: 'Titanic3',
-                    year: 1997,
-                    synopsys: 'James Cameron\'s "Titanic" is an epic, action-packed romance set against ... waters of the North Atlantic in the early hours of April 15, 1912.',
-                    gender:[],
-                    valueRating:7.9,
-                    ageRating:"+13",
-                    cast:[]
-                },
-                {
-                    filmId:4,
-                    filmImage: "https://static.euronews.com/articles/stories/07/31/01/20/606x758_cmsv2_dfdbec3d-af7a-5e75-9ae7-2ec0188cbf9a-7310120.jpg",
-                    name: 'Titanic4',
-                    year: 1997,
-                    synopsys: 'James Cameron\'s "Titanic" is an epic, action-packed romance set against ... waters of the North Atlantic in the early hours of April 15, 1912.',
-                    gender:[],
-                    valueRating:7.9,
-                    ageRating:"+13",
-                    cast:[]
-                },
-            ],
-        }
-    },
-    getters: {
-        getFilmsList: (state:State) => state.filmsList,
-    },
-    actions:{
-        postLike(){
-            //POST LIKE TO API
-            console.log("like")
-            this.filmsList.shift()
-        },
-        postDislike(){
-            //POST DISLIKE TO API
-            console.log("dislike")
-            this.filmsList.shift()
 
-        },
-        postSuperLike(){
-            //POST SUPERLIKE TO API
-            console.log("super")
-            this.filmsList.shift()
-        },
-        postUnwatch(){
-            //POST UNWATCHED TO API
-            console.log("No watch")
-            this.filmsList.shift()
-
-        }
-    }
-})
 
 export interface FilmInfo {
     name: string
     year: number
     gender: string []
-    filmImage:string
-    filmId:number
-    synopsys:string
-    cast:string[]
-    valueRating:number
-    ageRating:string
+    filmImage: string
+    filmId: number
+    synopsys: string
+    cast: string[]
+    valueRating: number
+    ageRating: string
 }
+
+enum Swipe {
+    Like = "Like",
+    Dislike = "Dislike",
+    Superlike = "Superlike",
+    Unwatch = "Unwatch",
+}
+
+interface FilmSwiped {
+    id: number;
+    state: Swipe;
+}
+
+interface State {
+    filmsList: FilmInfo[],
+    filmsToSend: FilmSwiped[]
+}
+
+export const useFilmStore = defineStore('film', {
+    state: (): State => {
+        return {
+            filmsToSend: [],
+            // for initially empty lists
+            filmsList: [
+                {
+                    filmId: 1,
+                    filmImage: "https://static.euronews.com/articles/stories/07/31/01/20/606x758_cmsv2_dfdbec3d-af7a-5e75-9ae7-2ec0188cbf9a-7310120.jpg",
+                    name: 'Titanic',
+                    year: 1997,
+                    synopsys: 'James Cameron\'s "Titanic" is an epic, action-packed romance set against ... waters of the North Atlantic in the early hours of April 15, 1912.',
+                    gender: [],
+                    valueRating: 7.9,
+                    ageRating: "+13",
+                    cast: []
+                },
+                {
+                    filmId: 2,
+                    filmImage: "https://static.euronews.com/articles/stories/07/31/01/20/606x758_cmsv2_dfdbec3d-af7a-5e75-9ae7-2ec0188cbf9a-7310120.jpg",
+                    name: 'Titanic2',
+                    year: 1997,
+                    synopsys: 'James Cameron\'s "Titanic" is an epic, action-packed romance set against ... waters of the North Atlantic in the early hours of April 15, 1912.',
+                    gender: [],
+                    valueRating: 7.9,
+                    ageRating: "+13",
+                    cast: []
+                },
+                {
+                    filmId: 3,
+                    filmImage: "https://static.euronews.com/articles/stories/07/31/01/20/606x758_cmsv2_dfdbec3d-af7a-5e75-9ae7-2ec0188cbf9a-7310120.jpg",
+                    name: 'Titanic3',
+                    year: 1997,
+                    synopsys: 'James Cameron\'s "Titanic" is an epic, action-packed romance set against ... waters of the North Atlantic in the early hours of April 15, 1912.',
+                    gender: [],
+                    valueRating: 7.9,
+                    ageRating: "+13",
+                    cast: []
+                },
+                {
+                    filmId: 4,
+                    filmImage: "https://static.euronews.com/articles/stories/07/31/01/20/606x758_cmsv2_dfdbec3d-af7a-5e75-9ae7-2ec0188cbf9a-7310120.jpg",
+                    name: 'Titanic4',
+                    year: 1997,
+                    synopsys: 'James Cameron\'s "Titanic" is an epic, action-packed romance set against ... waters of the North Atlantic in the early hours of April 15, 1912.',
+                    gender: [],
+                    valueRating: 7.9,
+                    ageRating: "+13",
+                    cast: []
+                },
+                {
+                    filmId: 1,
+                    filmImage: "https://static.euronews.com/articles/stories/07/31/01/20/606x758_cmsv2_dfdbec3d-af7a-5e75-9ae7-2ec0188cbf9a-7310120.jpg",
+                    name: 'Titanic',
+                    year: 1997,
+                    synopsys: 'James Cameron\'s "Titanic" is an epic, action-packed romance set against ... waters of the North Atlantic in the early hours of April 15, 1912.',
+                    gender: [],
+                    valueRating: 7.9,
+                    ageRating: "+13",
+                    cast: []
+                },
+                {
+                    filmId: 2,
+                    filmImage: "https://static.euronews.com/articles/stories/07/31/01/20/606x758_cmsv2_dfdbec3d-af7a-5e75-9ae7-2ec0188cbf9a-7310120.jpg",
+                    name: 'Titanic2',
+                    year: 1997,
+                    synopsys: 'James Cameron\'s "Titanic" is an epic, action-packed romance set against ... waters of the North Atlantic in the early hours of April 15, 1912.',
+                    gender: [],
+                    valueRating: 7.9,
+                    ageRating: "+13",
+                    cast: []
+                },
+                {
+                    filmId: 3,
+                    filmImage: "https://static.euronews.com/articles/stories/07/31/01/20/606x758_cmsv2_dfdbec3d-af7a-5e75-9ae7-2ec0188cbf9a-7310120.jpg",
+                    name: 'Titanic3',
+                    year: 1997,
+                    synopsys: 'James Cameron\'s "Titanic" is an epic, action-packed romance set against ... waters of the North Atlantic in the early hours of April 15, 1912.',
+                    gender: [],
+                    valueRating: 7.9,
+                    ageRating: "+13",
+                    cast: []
+                },
+                {
+                    filmId: 4,
+                    filmImage: "https://static.euronews.com/articles/stories/07/31/01/20/606x758_cmsv2_dfdbec3d-af7a-5e75-9ae7-2ec0188cbf9a-7310120.jpg",
+                    name: 'Titanic4',
+                    year: 1997,
+                    synopsys: 'James Cameron\'s "Titanic" is an epic, action-packed romance set against ... waters of the North Atlantic in the early hours of April 15, 1912.',
+                    gender: [],
+                    valueRating: 7.9,
+                    ageRating: "+13",
+                    cast: []
+                },
+                {
+                    filmId: 1,
+                    filmImage: "https://static.euronews.com/articles/stories/07/31/01/20/606x758_cmsv2_dfdbec3d-af7a-5e75-9ae7-2ec0188cbf9a-7310120.jpg",
+                    name: 'Titanic',
+                    year: 1997,
+                    synopsys: 'James Cameron\'s "Titanic" is an epic, action-packed romance set against ... waters of the North Atlantic in the early hours of April 15, 1912.',
+                    gender: [],
+                    valueRating: 7.9,
+                    ageRating: "+13",
+                    cast: []
+                },
+                {
+                    filmId: 2,
+                    filmImage: "https://static.euronews.com/articles/stories/07/31/01/20/606x758_cmsv2_dfdbec3d-af7a-5e75-9ae7-2ec0188cbf9a-7310120.jpg",
+                    name: 'Titanic2',
+                    year: 1997,
+                    synopsys: 'James Cameron\'s "Titanic" is an epic, action-packed romance set against ... waters of the North Atlantic in the early hours of April 15, 1912.',
+                    gender: [],
+                    valueRating: 7.9,
+                    ageRating: "+13",
+                    cast: []
+                },
+                {
+                    filmId: 3,
+                    filmImage: "https://static.euronews.com/articles/stories/07/31/01/20/606x758_cmsv2_dfdbec3d-af7a-5e75-9ae7-2ec0188cbf9a-7310120.jpg",
+                    name: 'Titanic3',
+                    year: 1997,
+                    synopsys: 'James Cameron\'s "Titanic" is an epic, action-packed romance set against ... waters of the North Atlantic in the early hours of April 15, 1912.',
+                    gender: [],
+                    valueRating: 7.9,
+                    ageRating: "+13",
+                    cast: []
+                },
+                {
+                    filmId: 4,
+                    filmImage: "https://static.euronews.com/articles/stories/07/31/01/20/606x758_cmsv2_dfdbec3d-af7a-5e75-9ae7-2ec0188cbf9a-7310120.jpg",
+                    name: 'Titanic4',
+                    year: 1997,
+                    synopsys: 'James Cameron\'s "Titanic" is an epic, action-packed romance set against ... waters of the North Atlantic in the early hours of April 15, 1912.',
+                    gender: [],
+                    valueRating: 7.9,
+                    ageRating: "+13",
+                    cast: []
+                },
+            ],
+        }
+    },
+    getters: {
+        getFilmsList: (state: State) => state.filmsList,
+    },
+    actions: {
+        postLike() {
+            this.postLiking(Swipe.Like)
+        },
+        postDislike() {
+            this.postLiking(Swipe.Dislike)
+        },
+        postSuperLike() {
+            this.postLiking(Swipe.Superlike)
+
+        },
+        postUnwatch() {
+            this.postLiking(Swipe.Unwatch)
+        },
+        postLiking(liking: Swipe) {
+            const filmId = this.filmsList.shift()?.filmId
+            if( filmId !== undefined){
+                const filmSwiped ={
+                    id:filmId,
+                    state:liking
+                }
+                this.filmsToSend.push(filmSwiped)
+            }
+            if(this.filmsToSend.length>=10){
+                //TODO Send data to BACK
+                console.log(this.filmsToSend)
+            }
+        }
+
+    }
+})
